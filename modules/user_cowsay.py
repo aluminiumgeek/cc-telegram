9be0656a475a1eebe2953dabcb1de6bc2db34e87
Mikhail Mezyakov <mihail265@gmail.com>
2015-12-13 15:25:58 +0300

    Do not process empty message

--- modules/user_cowsay.py
+++ modules/user_cowsay.py
@@ -10,6 +10,9 @@ def main(bot, *args):
     """
 
     text = clean_text(' '.join(args))
+    if not test.strip():
+        return
+
     process = os.popen(u'cowsay {}'.format(text))
     bot.send(text='```{}```'.format(process.read()), data={'parse_mode': 'Markdown'})
     process.close()
