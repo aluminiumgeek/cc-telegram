0bad854f21c265f8a64a7c1e30788f5274b06f99
Mikhail Mezyakov <mihail265@gmail.com>
2015-12-14 19:23:21 +0300

    Add `fortune` module

---
+++ modules/user_fortune.py
@@ -0,0 +1,22 @@
+import os
+import random
+
+
+def main(bot, *args):
+    """
+    fortune
+    Print a random, hopefully interesting, adage
+    """
+
+    fortunes_path = '/usr/share/games/fortunes/'
+
+    try:
+        types = filter(lambda x: not x.endswith('.dat'), os.listdir(fortunes_path))
+    except OSError:
+        return 'Package `fortunes` is not installed in your system. Run `aptitude install fortunes`'
+
+    fortune_file = random.choice(list(types))
+    with open(os.path.join(fortunes_path, fortune_file)) as fortune_file:
+        data = fortune_file.read()
+        quotes = data.split('%\n')
+        return random.choice(quotes)
