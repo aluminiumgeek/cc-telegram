343bf0a25add179bf9bbb9733efb6dc967a55767
aluminiumgeek <mihail265@gmail.com>
2016-03-21 18:21:57 +0000

    Add timeouts for all binary requests

--- modules/utils/data.py
+++ modules/utils/data.py
@@ -5,7 +5,7 @@ from requests.exceptions import RequestException
 
 def prepare_binary_from_url(url):
     try:
-        content = requests.get(url).content
+        content = requests.get(url, timeout=(1, 3)).content
     except RequestException:
         pass
     else:
