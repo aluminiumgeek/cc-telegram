65bb1ae1a1a52406f05b4eae0a707ab8b05b54b0
aluminiumgeek <mihail265@gmail.com>
2016-03-21 18:33:43 +0000

    Add redis settings

--- settings.py
+++ settings.py
@@ -3,6 +3,12 @@ token = '123456:AABCDEAABCDEABCDE1234ABCDE'
 name = 'shitsubot'
 owner = 'username'
 
+# Enable permanent redis store, otherwise temporary in-memory store will be used
+#redis_host =
+#redis_port =
+# You could connect to redis using unix socket instead of TCP
+#redis_socket =
+
 # Modules specific
 azure_bing_api_key = ''  # for `img` module
 google_api_key = ''  # for `g` module
