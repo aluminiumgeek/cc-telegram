b44ba104a61866682682e5167a52386b4fccd8d7
aluminiumgeek <mihail265@gmail.com>
2016-03-30 07:42:27 +0000

    Add `start` module

---
+++ modules/user_start.py
@@ -0,0 +1,10 @@
+async def main(bot, *args, **kwargs):
+    """
+    start
+    Introduction to this bot
+    """
+    return 'Welcome to Shitsu bot.\n'\
+           'Check available commands: /lsmod\n'\
+           'Get help on a command: /man <command>\n\n'\
+           'The bot is named after Shitsu from Code Geass.\n'\
+           'Source code is available here: https://github.com/aluminiumgeek/cc-telegram'
