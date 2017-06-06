9ab9a069ac4a98397ab75de5719a9b95292f3563
Mikhail Mezyakov <mihail265@gmail.com>
2017-06-06 21:58:24 +0400

    Add webm -> mp4 converter

---
+++ modules/text_webm_preview.py
@@ -0,0 +1,48 @@
+import os
+import re
+
+from moviepy.editor import *
+
+from modules.utils.data import download_file
+
+
+def main(bot, message, update, *args, **kwargs):
+    chat_id = kwargs.get('chat_id')
+    message = message.strip()
+    message_id = update.get('message', {}).get('message_id')
+    for url in re.findall(r'http.*\.webm', message):
+        file_id = bot.store.get(url)
+
+        data = {
+            'chat_id': chat_id,
+            'reply_to_message_id': message_id,
+            'disable_notification': True
+        }
+
+        if file_id:
+            data.update(video=file_id)
+            files = None
+        else:
+            file_name = download_file(url)
+            if not file_name:
+                return
+            clip = VideoFileClip(file_name)
+            clip_filename = '{}.mp4'.format(file_name)
+
+            bot.pre_send(chat_id=chat_id, action='upload_video')
+
+            clip.write_videofile(clip_filename, threads=4, preset='superfast')
+
+            data.update(width=clip.w, height=clip.h, duration=clip.duration)
+            files = {'video': (clip_filename, open(clip_filename, 'rb'), 'video/mp4')}
+
+        telegram_response = bot.call('sendVideo', 'POST', data=data, files=files)
+
+        if telegram_response and telegram_response.get('video') and not file_id and bot.store:
+            bot.store.set(url, telegram_response['video']['file_id'])
+
+        if not file_id:
+            os.remove(file_name)
+            os.remove(clip_filename)
+
+main.prevent_pre_send = True
--- modules/utils/data.py
+++ modules/utils/data.py
@@ -1,5 +1,8 @@
-from io import BytesIO
 import requests
+import shutil
+import uuid
+from io import BytesIO
+
 from requests.exceptions import RequestException
 
 
@@ -10,3 +13,18 @@ def prepare_binary_from_url(url):
         pass
     else:
         return BytesIO(content)
+
+
+def download_file(url, max_length=20 * 1024 * 1024):
+    try:
+        length = requests.head(url).headers.get('content-length', 0)
+        if int(length) > max_length:
+            return
+        r = requests.get(url, timeout=(1, 3), stream=True)
+        ext = url.rsplit('.', 1)[1]
+        file_name = '{}.{}'.format(uuid.uuid4(), ext)
+        with open(file_name, 'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+        return file_name
+    except RequestException:
+        return
