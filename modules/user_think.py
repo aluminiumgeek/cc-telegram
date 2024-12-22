7b97cfb789d44bf78700920cc9452cbec86a7e82
HooFoo <lhoofool@gmail.com>
2024-12-22 22:30:16 +0400

    Cheap model

--- modules/user_think.py
+++ modules/user_think.py
@@ -20,7 +20,7 @@ async def main(bot, *args, **kwargs):
             'Content-Type': 'application/json'
         }
         data = {
-            'model': 'gpt-4-mini',
+            'model': 'gpt-4o-mini',
             'messages': [{'role': 'system', 'content': system_prompt}, {'role': 'user', 'content': prompt}],
             'temperature': 0.6,
             'presence_penalty': 0.5,
